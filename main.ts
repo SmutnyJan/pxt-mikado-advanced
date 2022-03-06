092d79e07cecb3e26400f0654176bf468827b031
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-03-06 16:21:15 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,49 @@
 let jeZapnutoHlidani = false
+let akcelerace = 0
+let stavDisleje = ""
 input.onButtonPressed(Button.A, function () {
     if (jeZapnutoHlidani == false) {
-        Mikado.zapnoutHlidani()
         jeZapnutoHlidani = true
     } else {
-        Mikado.vypnoutHlidani()
         jeZapnutoHlidani = false
     }
 })
 function detekujPohyb (tolerance: number) {
-    if (input.acceleration(Dimension.Strength) + tolerance < Mikado.klidovaHodnota() || input.acceleration(Dimension.Strength) - tolerance > Mikado.klidovaHodnota()) {
+    akcelerace = input.acceleration(Dimension.Strength)
+    if (akcelerace + tolerance < Mikado.klidovaHodnota() || akcelerace - tolerance > Mikado.klidovaHodnota()) {
         return true
     } else {
         return false
     }
 }
 basic.forever(function () {
-    if (detekujPohyb(100) == true && jeZapnutoHlidani) {
-        Mikado.vzbuditHlidace()
+    if (detekujPohyb(500) == true && jeZapnutoHlidani && stavDisleje != "nastvany") {
+        stavDisleje = "nastvany"
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        soundExpression.sad.playUntilDone()
+    } else if (jeZapnutoHlidani && stavDisleje != "pozor") {
+        stavDisleje = "pozor"
+        basic.showLeds(`
+            # # . # #
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    } else if (!(jeZapnutoHlidani) && stavDisleje != "stastny") {
+        stavDisleje = "stastny"
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
     }
 })
